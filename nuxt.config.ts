b3b80ad9584ae6ecfdb07594e8287934e0f52597
fufu-girl-meow <furries@duck.com>
2025-06-07 11:23:45 +0700

    Tailwind CSS Init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
+  css: ['~/public/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  }
 });
