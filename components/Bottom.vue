d664e15e7175d2572a5ed31d426c64976cfa3e22
lovelytransgirl <furries@duck.com>
2025-06-10 22:20:04 +0700

    big enough

--- components/Bottom.vue
+++ components/Bottom.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
-        <div class="mt-10 mx-auto mb-10 backdrop-blur-sm bg-new-pink/30 text-white p-4 rounded-full shadow-md w-full max-w-xl ring-4 ring-new-purple/30 flex justify-center items-center">
+        <div class="mt-10 mx-auto mb-10 backdrop-blur-sm bg-new-pink/30 text-white p-6 rounded-full shadow-md w-full max-w-xl ring-4 ring-new-purple/30 flex justify-center items-center">
             <div class="flex flex-col items-center justify-center w-full">
                 <div class="flex flex-wrap sm:flex-nowrap items-center justify-center gap-2 sm:gap-4">
-                    <a href="https://github.com/fufu-girl-meow/fufu.transdev.pw" class="text-shadow-lg/40 text-sm text-new-pink text-shadow-new-dark-purple">
+                    <a href="https://github.com/fufu-girl-meow/fufu.transdev.pw" class="text-shadow-lg/40 text-medium text-new-pink text-shadow-new-dark-purple">
                         Repository
                     </a>
-                    <a href="https://github.com/fufu-girl-meow/" class="text-shadow-lg/40 text-sm text-new-pink text-shadow-new-dark-purple">
+                    <a href="https://github.com/fufu-girl-meow/" class="text-shadow-lg/40 text-medium text-new-pink text-shadow-new-dark-purple">
                       Github
                     </a>
-                    <a href="/blog" class="text-shadow-lg/40 text-sm text-new-pink text-shadow-new-dark-purple">
+                    <a href="/blog" class="text-shadow-lg/40 text-medium text-new-pink text-shadow-new-dark-purple">
                       Blog
                     </a>
                 </div>
